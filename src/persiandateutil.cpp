74864cc2f6725ef56cc8291ed00207db43377f46
Reza Alizadeh Majd <r.a.majd@gmail.com>
2012-07-15 09:47:11 +0430

    - Add PersianDateUtils class to handle PersianDate Ralated Works (Converting Date, ...)

---
+++ src/persiandateutil.cpp
@@ -0,0 +1,5 @@
+#include "persiandateutil.h"
+
+PersianDateUtil::PersianDateUtil()
+{
+}
---
+++ src/persiandateutil.h
@@ -0,0 +1,18 @@
+#ifndef PERSIANDATEUTIL_H
+#define PERSIANDATEUTIL_H
+
+
+typedef struct _ShamsiDate
+{
+    int Year;
+    int Month;
+    int Day;
+}ShamsiDate;
+
+class PersianDateUtil
+{
+public:
+    PersianDateUtil();
+};
+
+#endif // PERSIANDATEUTIL_H
